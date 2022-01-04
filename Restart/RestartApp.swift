80a4357526c1e5076f10dd36dc4a27fdd2feed33
Andrew Irvine <adi356@umsystem.edu>
2022-01-04 10:06:09 -0700

    Testing git setup in xcode

--- Restart/RestartApp.swift
+++ Restart/RestartApp.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Andrew Irvine on 11/4/21.
 //  Basic app that makes use of an onboarding screen, complex gestures (slide to start, horizontal parallax effect, etc.), and animation (fade in/fade out, scaling circle, lady meditating, opacity transition), and has haptic feedback as well as audio
+// extra text to test git setup
 
 import SwiftUI
 
